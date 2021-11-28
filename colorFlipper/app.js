8318ef3102dcd1635fac933c5bb8c33c61e7bd50
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-29 01:47:33 +0400

    adding functionality to the color flipper project

--- colorFlipper/app.js
+++ colorFlipper/app.js
@@ -1,1 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// declaring array
+const colors = ["green", "red", "blue", "orange", "pink", "yellow"];
+
+// selecting elements
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// adding event listner to the button
+btn.addEventListener("click", function () {
+  let randomNumber = randomNumberGenerator();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.style.color = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+// using math floor and random methdos to generate random numbers between 0 and the lenght of colors array
+function randomNumberGenerator() {
+  return Math.floor(Math.random() * colors.length);
+}
