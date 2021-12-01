624634ffd83827736fc41f3e4805bfcb0e5fa41e
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-01 14:50:46 +0400

    working with date object in countdown project

--- countdown/app.js
+++ countdown/app.js
@@ -22,3 +22,17 @@ const weekdays = [
   "Saturday",
 ];
 
+const giveway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+// working with date onject
+let futureDate = new Date(2021, 11, 23, 16, 10);
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const day = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+
+giveway.innerHTML = `giveaway ends on ${weekday}, ${day} ${month} ${year} ${hour}:${minutes}pm`;
