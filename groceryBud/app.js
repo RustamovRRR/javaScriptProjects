29ce48a6e44e08634b0e22738af2939604759cda
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-02 15:40:37 +0400

    adding event listner to the form and creating additem function

--- groceryBud/app.js
+++ groceryBud/app.js
@@ -1,7 +1,7 @@
 // ****** SELECT ITEMS **********
 const alert = document.querySelector(".alert");
 const form = document.querySelector(".grocery-form");
-const grocery = document.getElementById(".grocery");
+const grocery = document.getElementById("grocery");
 const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
@@ -13,9 +13,23 @@ let editFlag = false;
 let editId = "false";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
+  if (value && !editFlag) {
+    console.log("add item");
+  } else if (value && editFlag) {
+    console.log("edit");
+  } else {
+    console.log("empty");
+  }
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
