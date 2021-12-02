76339b5d1420ee0386ed01504d1cbed7bc063f2d
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-03 01:21:22 +0400

    adding prev and next btns functionality

--- sliderProject/app.js
+++ sliderProject/app.js
@@ -1,7 +1,31 @@
-const slided = document.querySelectorAll(".slide");
-const prevBtn = document.querySelector(".prevBtn");
+const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
-
-slided.forEach(function (slide, index) {
+const prevBtn = document.querySelector(".prevBtn");
+slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
+
+let counter = 0;
+
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
